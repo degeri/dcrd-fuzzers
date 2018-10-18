a44f9d225a17e501d825f4a103fc6bc9e8816d01
David Hill <dhill@mindcry.org>
2018-10-18 14:34:57 -0400

    stake: initial commit

---
+++ stake/fuzz.go
@@ -0,0 +1,14 @@
+package stake
+
+import (
+	dcrd_stake "github.com/decred/dcrd/blockchain/stake"
+)
+
+func Fuzz(input []byte) {
+	outputs := []*dcrd_stake.MinimalOutput{
+		{
+			PkScript: input,
+		},
+	}
+	dcrd_stake.SStxStakeOutputInfo(outputs)
+}
